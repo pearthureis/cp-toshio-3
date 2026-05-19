0b4a4a87331b6c4bf156ce72fe9b60e28b4d9b4e
eduardonovaki <eduardo.novaki01@gmail.com>
2026-05-19 11:07:54 -0300

    funcao consultarsaldo feita e o switch case feito tbm

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void consultarSaldo(float saldo) {
+printf("\n--- SALDO ATUAL ---\n");
+printf("R$ %.2f\n", saldo);
+printf("-------------------\n");
+}
+
 void exibirMenu() {
 printf("\n========================\n");
 printf(" CAIXA ELETRONICO \n");
@@ -15,11 +21,29 @@ printf("Escolha uma opcao: ");
 
 int main(){
     int opcao;
+    float saldo = 420;
     do {
     system("cls");
     exibirMenu();
     scanf("%d", &opcao);
-    } while(opcao != 0);
-    printf("Obrigado por usar nosso ATM!");
-
+    } while(opcao >=0 && opcao <= 3);
+    if (opcao > 3 || opcao < 0){
+        system("cls");
+        printf("valor digitado invalido!! \n");
+        exibirMenu();
+        scanf("%d", &opcao);
+    }
+    switch(opcao) {
+        case 1:
+        consultarSaldo(saldo);
+        break;
+        case 2:
+        saldo = realizarSaque(saldo);
+        break;
+        case 0:
+        printf("Saindo...\n");
+        break;
+        default:
+        printf("Opcao Invalida!\n");
+        }
 }
